0f679937d0cd829d5d71455977b40aa931b8b66f
Vince Andrew Pimentel <contact@vincep.me>
2018-10-03 06:35:13 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 # code your #position_taken? method here!
 def position_taken?(array, index)
-  if array[index] == " " || array[index] == ""
+  if array[index] == " " || array[index] == "" || array[index] == nil
   end
 end
