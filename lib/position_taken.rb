a0dc926e9e5836e4cf81a6ce96063394d17d0b25
Vince Andrew Pimentel <contact@vincep.me>
2018-10-03 06:37:41 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,7 @@
 def position_taken?(array, index)
   if array[index] == " " || array[index] == "" || array[index] == nil
     false
+  elsif array[index] == "X" || array[index] == "O"
+    true
   end
 end
